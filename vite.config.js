f552e5b146aa8ecb21b8340982ca272967520070
vikkkaa22 <kusya2203@gmail.com>
2025-09-12 12:48:50 +0500

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
     return {
+        base:"Memory-Game-Vite",
         plugins: [
             ViteImageOptimizer({
                 png: {
