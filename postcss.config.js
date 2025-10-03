9cec84846d7d6d76aef0b386636f8a67009af770
vikkkaa22 <kusya2203@gmail.com>
2025-10-03 11:15:18 +0500

    wrking saas 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 import autoprefixer from "autoprefixer";
 import { plugin } from "postcss";
 
-export default{
-    plugin:{
-        autoprefixer:{
-            overrideBrowserlist: ['>0.1%', 'last 2 versions', 'nod dead']
-        }
-    }
-}
+export default {
+    plugins: {
+        autoprefixer: {
+            overrideBrowserslist: ['> 0.1%', 'last 2 versions', 'not dead'],
+        },
+    },
+};
+
